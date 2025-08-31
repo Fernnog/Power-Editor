3146982c4f85faf59b04639a72a4ab1e6aab2b98
Fernnog <fernandocnogueira@gmail.com>
2025-08-30 22:20:09 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+    // IMPORTANTE: Cole sua chave de API do Google AI Studio aqui, dentro das aspas.
+    // Exemplo: apiKey: "AIzaSyB..._wX4"
+    apiKey: "AIzaSyDKtcjpegb3wbSM8QoBrNfbs1bD7p-HXzg"
+};
