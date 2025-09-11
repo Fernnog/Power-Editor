2ef9641231594b0f2bbca0505d666358e1bdbca0
Fernnog <fernandocnogueira@gmail.com>
2025-09-11 13:35:45 -0400

    Update ui-icons.js

--- js/ui-icons.js
+++ js/ui-icons.js
@@ -1,3 +1,5 @@
+// --- START OF FILE ui-icons.js ---
+
 // --- ÍCONES DA SIDEBAR E AÇÕES DE MODELO ---
 // Estilo unificado: stroke-based, 1.75px de espessura
 const ICON_TRASH = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.75"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>`;
