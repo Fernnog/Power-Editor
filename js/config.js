769aba37aef5a157c98712ece89b7f1cf2cb7372
Fernnog <fernandocnogueira@gmail.com>
2025-12-06 18:46:26 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,5 @@ const CONFIG = {
     // Sua chave de API do Google AI Studio
     apiKey: "", // Mantive a do seu log (mas cuidado ao expor!)
 
-    // ALTERAÇÃO: Adicionado sufixo '-latest' para garantir que a API encontre o modelo
-    model: "gemini-2.0-flash-exp"
+    model: "gemini-flash-latest"
 };
