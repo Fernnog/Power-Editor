1dca60c7caa478e5b07da09a0e913b10256c1d4e
Fernnog <fernandocnogueira@gmail.com>
2025-12-06 17:06:29 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ const CONFIG = {
     apiKey: "AIzaSyC0EiIzNQqMyOCI5oV2XXQvGzVFntFaqQU", // Mantive a do seu log (mas cuidado ao expor!)
 
     // ALTERAÇÃO: Adicionado sufixo '-latest' para garantir que a API encontre o modelo
-    model: "gemini-1.5-flash-latest"
+    model: "gemini-1.5-flash"
 };
